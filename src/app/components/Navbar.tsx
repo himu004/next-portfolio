83c617bda6069b9d06459145d3e0d92d9af78a5b
Himu Chowdhury <himu.chowdhury01@gmail.com>
2024-05-11 23:19:31 +0600

    Next Portfolio Project Added

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,8 @@
+'use client'
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Home() {
+  return (
+    <h1 className="text-center text-xl">Ayaaz Cowdhury Care of Himu Chowdhury</h1>
+  );
+}
