8644f3c1c050e61192655bce991b2621e0dd4774
Himu Chowdhury <himu.chowdhury01@gmail.com>
2024-05-13 00:03:55 +0600

    Featured Courses Section Added

---
+++ src/app/components/FeaturedCourses.tsx
@@ -0,0 +1,21 @@
+import courseData from "../../data/music_courses.json"
+function FeaturedCourses() {
+  return (
+    <div className="py-12 bg-gray-900">
+        <div>
+            <div className="text-center">
+                <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">Featured Courses</h2>
+                <p>Learn with the Best</p>
+            </div>
+        </div>
+        <div>
+            2
+        </div>
+        <div>
+            3
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedCourses
